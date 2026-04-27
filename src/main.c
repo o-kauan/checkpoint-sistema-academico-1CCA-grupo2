1fd6f9a191cee8837da71c596c94e4a8c817df9d
crfvelloso <guilhermevelloso68@gmail.com>
2026-04-27 10:53:01 -0300

    Adição do Menu (Sem funcionar)

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 
 int main() {
-    
+    printf("===========================================\n");
+    printf("            SISTEMA EQUIPE 2               \n");
+    printf("===========================================\n");
+    printf("\n1 - Inserir Notas ");
+    printf("\n2 - Calcular Media ");
+    printf("\n3 - Verificar Situacao ");
+    printf("\n4 - Exibir Resultado ");
+    printf("\n5 - Calcular Derivada ");
+    printf("\n6 - Sair ");
+    printf("\n\n Escolha uma Opcao: ");
 
     
     return 0;
