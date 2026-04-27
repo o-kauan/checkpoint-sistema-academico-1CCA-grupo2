a167acc3f2e01a82c3bcb50cc953ecb7050cb477
Kauan <rm573727@fiap.com.br>
2026-04-27 11:22:30 -0300

    add função derivada

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 
+void derivada(){
+    float a, b ,c;
+
+    printf("\nDigite o coeficiente a: ");
+    scanf("%f", &a);
+    printf("\nDigite o coeficiente b: ");
+    scanf("%f", &b);
+    printf("\nDigite o coeficiente c: ");
+    scanf("%f", &c);
+}
+
 int main() {
     printf("===========================================\n");
     printf("            SISTEMA EQUIPE 2               \n");
@@ -9,6 +20,8 @@ int main() {
     printf("\n3 - Verificar Situacao ");
     printf("\n4 - Exibir Resultado ");
     printf("\n5 - Calcular Derivada ");
+    derivada();
+
     printf("\n6 - Sair ");
     printf("\n\n Escolha uma Opcao: ");
 
